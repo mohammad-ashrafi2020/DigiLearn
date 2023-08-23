55a8fa7e06a2372a60cd4856e1df18dfd755d9e9
Mohammad Ashrafi <69156294+mohammad-ashrafi2020@users.noreply.github.com>
2023-08-23 18:52:39 +0330

    Creatre New Blog Post ( in Admin Panel )

--- src/EndPoints/DigiLearn.Web/Areas/Admin/Pages/Blog/Posts/Index.cshtml.cs
+++ src/EndPoints/DigiLearn.Web/Areas/Admin/Pages/Blog/Posts/Index.cshtml.cs
@@ -1,27 +1,88 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 using BlogModule.Services;
+using BlogModule.Services.DTOs.Command;
 using BlogModule.Services.DTOs.Query;
+using DigiLearn.Web.Infrastructure;
 using DigiLearn.Web.Infrastructure.RazorUtils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using UserModule.Core.Services;
 
-namespace DigiLearn.Web.Areas.Admin.Pages.Blog.Posts
+namespace DigiLearn.Web.Areas.Admin.Pages.Blog.Posts;
+
+public class IndexModel : BaseRazorFilter<BlogPostFilterParams>
 {
-    public class IndexModel : BaseRazorFilter<BlogPostFilterParams>
+    private IBlogService _blogService;
+    private IUserFacade _userFacade;
+    public IndexModel(IBlogService blogService, IUserFacade userFacade)
     {
-        private IBlogService _blogService;
+        _blogService = blogService;
+        _userFacade = userFacade;
+    }
 
-        public IndexModel(IBlogService blogService)
+
+    public List<BlogCategoryDto> Categories { get; set; }
+    public BlogPostFilterResult FilterResult { get; set; }
+
+
+
+    [Display(Name = "عنوان")]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    [BindProperty]
+    public string Title { get; set; }
+
+    [Display(Name = "نام نویسنده")]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    [BindProperty]
+    public string OwnerName { get; set; }
+
+    [Display(Name = "توضیحات")]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    [UIHint("Ckeditor4")]
+    [BindProperty]
+    public string Description { get; set; }
+
+    [Display(Name = "slug")]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    [BindProperty]
+    public string Slug { get; set; }
+
+    [Display(Name = "عکس مقاله")]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    [BindProperty]
+    public IFormFile ImageFile { get; set; }
+
+
+    [Display(Name = "دسته بندی")]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    [BindProperty]
+    public Guid CategoryId { get; set; }
+
+    public async Task OnGet()
+    {
+        Categories = await _blogService.GetAllCategories();
+        FilterResult = await _blogService.GetPostsByFilter(FilterParams);
+
+        var user = await _userFacade.GetUserByPhoneNumber(User.GetPhoneNumber());
+        if (user != null)
         {
-            _blogService = blogService;
+            OwnerName = user.Name + " " + user.Family;
         }
+    }
 
-
-        public List<BlogCategoryDto> Categories { get; set; }
-        public BlogPostFilterResult FilterResult { get; set; }
-        public async Task OnGet()
+    public async Task<IActionResult> OnPost()
+    {
+        var result = await _blogService.CreatePost(new CreatePostCommand()
         {
-            Categories = await _blogService.GetAllCategories();
-            FilterResult = await _blogService.GetPostsByFilter(FilterParams);
-        }
+            UserId = User.GetUserId(),
+            OwnerName = OwnerName,
+            CategoryId = CategoryId,
+            Description = Description,
+            Slug = Slug,
+            Title = Title,
+            ImageFile = ImageFile
+        });
+        return RedirectAndShowAlert(result, RedirectToPage("Index"));
     }
 }
